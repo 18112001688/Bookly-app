66d943defa9511f917fe9b46ff9e3850de0ca4cc
cipher <moustafasaper167@gmail.com>
2023-04-25 07:28:39 +0200

    adding animation to text in splash view

--- lib/core/assets.dart
+++ lib/core/assets.dart
@@ -1,7 +1,4 @@
 class AssetsData {
-  static const iconOne = 'assets/icons/icon_one.gif';
-  static const iconThree = 'assets/icons/icon_three.gif';
-  static const iconTwo = 'assets/icons/icon_two.gif';
   static const bookLover = 'assets/icons/book_lover.png';
   static const booklyImage = 'assets/icons/Logo.png';
 }
