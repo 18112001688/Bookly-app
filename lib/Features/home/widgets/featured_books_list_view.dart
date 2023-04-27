8873f0abb867fcf5b112229815a859691947e6f8
cipher <moustafasaper167@gmail.com>
2023-04-27 08:32:31 +0200

    list view to build the item is added

---
+++ lib/Features/home/widgets/featured_books_list_view.dart
@@ -0,0 +1,23 @@
+import 'package:bookly_app/Features/home/widgets/coustem_list_view_item.dart';
+import 'package:flutter/material.dart';
+
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: 10,
+            itemBuilder: (context, index) => const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0),
+                  child: CoustemListViewItem(),
+                )),
+      ),
+    );
+  }
+}
--- lib/Features/home/widgets/home_view_body.dart
+++ lib/Features/home/widgets/home_view_body.dart
@@ -1,6 +1,5 @@
 import 'package:bookly_app/Features/home/widgets/coustem_app_bar.dart';
-import 'package:bookly_app/Features/home/widgets/coustem_list_view_item.dart';
-import 'package:bookly_app/core/assets.dart';
+import 'package:bookly_app/Features/home/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -9,10 +8,7 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: const [
-        CoustemAppBar(),
-        CoustemListViewItem(),
-      ],
+      children: const [CoustemAppBar(), FeaturedBooksListView()],
     );
   }
 }
