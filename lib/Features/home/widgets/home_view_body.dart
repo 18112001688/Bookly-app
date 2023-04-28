34c10f60b09d479d25972eee4a433b76573296f9
cipher <moustafasaper167@gmail.com>
2023-04-28 09:09:06 +0300

    Best seller item part 1

--- lib/Features/home/widgets/home_view_body.dart
+++ lib/Features/home/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/Features/home/widgets/coustem_app_bar.dart';
 import 'package:bookly_app/Features/home/widgets/featured_books_list_view.dart';
+import 'package:bookly_app/core/assets.dart';
 import 'package:bookly_app/core/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -22,8 +23,42 @@ class HomeViewBody extends StatelessWidget {
             'Best Seller',
             style: Styles.meduimText,
           ),
-        )
+        ),
+        BestSellerListViewItem(),
       ],
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 18),
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 2.7 / 4,
+              child: Container(
+                decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                    image: DecorationImage(
+                        image: AssetImage(
+                          (AssetsData.testImage),
+                        ),
+                        fit: BoxFit.fill)),
+              ),
+            ),
+            Column(
+              children: const [],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
