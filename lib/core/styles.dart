b1a95f60b75ed481abc0c01771dbce70d3a2abca
cipher <moustafasaper167@gmail.com>
2023-04-29 09:45:48 +0300

    create text styles

--- lib/core/styles.dart
+++ lib/core/styles.dart
@@ -1,8 +1,23 @@
+import 'package:bookly_app/Features/constant.dart';
 import 'package:flutter/material.dart';
 
 abstract class Styles {
-  static const meduimText = TextStyle(
+  static const textStyle18 = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.w600,
+  );
+
+  static const styleText20 = TextStyle(
+      fontSize: 20, fontWeight: FontWeight.normal, fontFamily: kGtsectraFine);
+
+  static const styleText30 = TextStyle(
+      fontSize: 30, fontWeight: FontWeight.w600, fontFamily: kGtsectraFine);
+  static const styleText14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+  static const styleText16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
   );
 }
