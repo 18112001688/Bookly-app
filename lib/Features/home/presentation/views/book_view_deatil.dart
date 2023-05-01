f874215bdd40bae39c5bc63b629fbbe7fa44aa48
cipher <moustafasaper167@gmail.com>
2023-05-01 12:20:26 +0300

    craete books details view

---
+++ lib/Features/home/presentation/views/book_view_deatil.dart
@@ -0,0 +1,15 @@
+import 'package:bookly_app/Features/home/widgets/book_view_datails_body.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class BookDetailsView extends StatelessWidget {
+  const BookDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BookViewDeatailsBody(),
+    );
+  }
+}
---
+++ lib/Features/home/widgets/book_view_datails_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BookViewDeatailsBody extends StatelessWidget {
+  const BookViewDeatailsBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
