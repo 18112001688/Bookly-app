2514439e233128615051da8a551ef80f4ad3d744
cipher <moustafasaper167@gmail.com>
2023-05-07 16:30:05 +0300

    create home repo method and failure file

---
+++ lib/Features/home/data/repos/home_repo.dart
@@ -0,0 +1,10 @@
+import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/core/errors/faluires.dart';
+import 'package:dartz/dartz.dart';
+
+// it's a class we build to determine what method should be executed for each feature
+abstract class HomeRepo {
+  // eithr gives you two options to return
+  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+}
