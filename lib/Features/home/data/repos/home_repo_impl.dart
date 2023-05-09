d9b3248430625b8fd1f9f80c475ccd75291db903
cipher <moustafasaper167@gmail.com>
2023-05-09 13:54:33 +0300

    implement fetchNewsetBooks service

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -1,14 +1,30 @@
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
+import 'package:bookly_app/core/utlity/api_services.dart';
 import 'package:dartz/dartz.dart';
 import 'package:bookly_app/core/errors/faluires.dart';
-import 'package:dio/dio.dart';
 
 class HomeRepoImp implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImp(this.apiService);
+
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    Dio().get('path');
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewsetBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint: 'volumes?Sorting=newest &q=subject:Life');
+
+      // parse the data that comes from the api to are books empty list
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } catch (e) {
+      return left(ServerFilauire());
+    }
   }
 
   @override
