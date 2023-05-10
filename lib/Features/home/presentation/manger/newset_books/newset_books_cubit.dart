5b38d130c5639d14352143c81b50a0dc41c2ce34
cipher <moustafasaper167@gmail.com>
2023-05-10 13:54:20 +0300

    newset cubit is finished

--- lib/Features/home/presentation/manger/newset_books/newset_books_cubit.dart
+++ lib/Features/home/presentation/manger/newset_books/newset_books_cubit.dart
@@ -1,9 +1,23 @@
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future fetchNewsetBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchNewsetBooks();
+    // fold is mehod inside the either class return the two (Left and right)
+    result.fold((failure) {
+      emit(NewsetBooksFailure(failure.errorMessage));
+    }, (books) {
+      emit(NewsetBooksSuccess(books));
+    });
+  }
 }
