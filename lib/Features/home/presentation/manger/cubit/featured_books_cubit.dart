23e59de674bbeacb453bdbe76acdb760e78887d4
cipher <moustafasaper167@gmail.com>
2023-05-12 16:21:33 +0300

    start

--- lib/Features/home/presentation/manger/cubit/featured_books_cubit.dart
+++
@@ -1,6 +0,0 @@
-import 'package:bookly_app/Features/home/presentation/manger/cubit/featured_books_state.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
-}
